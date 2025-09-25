d1a30af97b14a3fc1af7c179b178f5bfc74270a5
machodev71 <machodev71@gmail.com>
2025-09-25 08:25:38 +0530

    i

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask App Running on Dokploy 🚀"
+    return "i love pavi "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
